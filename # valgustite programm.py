bbcd15f0d71169f0b69cb3df2ad9719221a77921
Isometimescode1 <66833302+Isometimescode1@users.noreply.github.com>
2022-12-08 16:00:28 +0200

    Create # valgustite programm.py
    Täiendamisel

---
+++ # valgustite programm.py
@@ -0,0 +1,8 @@
+# valgustite programm
+# 7/12/2022 EL
+# raspi peal: pip3 install rpi_ws281x adafruit-circuitpython-neopixel
+# python3 -m pip install --force-reinstall adafruit-blinka
+
+import time
+import board
+import neopixel
