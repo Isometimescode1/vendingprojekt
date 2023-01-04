8d21300e4d0114bb2ab6b02df84e16d7aa98fecd
Hardi1234 <hardi.tambets@gmail.com>
2023-01-04 17:18:42 +0000

    leds somewhat work now

--- hwtest.py
+++ hwtest.py
@@ -5,15 +5,18 @@
 # Kuupäev:      12.2022
 #
 
-#import pigpio
+import pigpio
 #import sys
-import hardware as hw
+#import hardware as hw
 from time import sleep
+
+import leds
 
 LIL_LAG = 0         #kui raspi baud rate-i ei muuda on mingit aeglustust vaja attiny numpadi küljes ei saa muidu hakka
 while(1):
-    input = hw.get_digit()
-    if input == "#":
-        break
+    leds.rainbow_cycle(0.001)
+
+    sleep(1)
+
     print(input)
     
